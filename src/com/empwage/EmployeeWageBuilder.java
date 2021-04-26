242b1943864d1c355d72a39e9718af251fda397e
Pratap Pawar <pratappawar1997@gmail.com>
2021-04-26 21:47:09 +0530

    Added: Employee Attendance Status Added

--- src/com/empwage/EmployeeWageBuilder.java
+++ src/com/empwage/EmployeeWageBuilder.java
@@ -4,6 +4,17 @@ public class EmployeeWageBuilder {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation !!");
-	}
+		
+		int is_Full_Time=1;
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if(empCheck==is_Full_Time) {
+			System.out.println("Employee is Present..");
+		}
+		else
+			System.out.println("Employee is Absent..");
 
+	}
+		
 }
+
+
