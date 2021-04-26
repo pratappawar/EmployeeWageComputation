e1a49d8bf88757e7eb0630d1e8bbc86b6ff5d7ec
Pratap Pawar <pratappawar1997@gmail.com>
2021-04-26 22:15:20 +0530

    Added: Part time Employee and wage Calculation

--- src/com/empwage/EmployeeWageBuilder.java
+++ src/com/empwage/EmployeeWageBuilder.java
@@ -8,13 +8,16 @@ public class EmployeeWageBuilder {
 		int is_Full_Time=1;
 		int wageperHr=20;
 		int FullDayHr=8;
+		int HalfDayHr=4;
 		double empCheck=Math.floor(Math.random()*10)%2;
 		if(empCheck==is_Full_Time) {
 			System.out.println("Employee is Present..");
 			System.out.println("Employee Daily Wage is="+(wageperHr*FullDayHr));
 		}
-		else
-			System.out.println("Employee is Absent..");
+		else {
+			System.out.println("Employee Daily Half_Day Wage is="+(wageperHr*HalfDayHr));
+		}
+			
 	}
 		
 }
