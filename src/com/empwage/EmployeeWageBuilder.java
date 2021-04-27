8dd0809040d441c773c9a4030e7b32fbfa56cceb
Pratap Pawar <pratappawar1997@gmail.com>
2021-04-27 14:55:24 +0530

    Added: Calculate Emp Wage Till Codition

--- src/com/empwage/EmployeeWageBuilder.java
+++ src/com/empwage/EmployeeWageBuilder.java
@@ -16,6 +16,7 @@ public class EmployeeWageBuilder {
 		int totalWorkingHr=0;
 		int totalWorkingDays=0;
 		int FullTimeDay=0;
+		int absent=0;
 		int partTimeDay=0;
 		double empCheck=Math.floor(Math.random()*10)%3;
 		if(empCheck==is_Full_Time) {
@@ -38,19 +39,25 @@ public class EmployeeWageBuilder {
 				break;
 			case 1:
 				FullTimeDay++;
-				totalEmpHr=totalEmpHr+FullDayHr;
+				totalEmpHr=totalEmpHr+(FullDayHr/2);
 				break;
 			default:
-				System.out.println("Employee is Absent !!!");
+				absent++;
 				break;
 			}			
 		}
 		int full_time_incom=wageperHr*(FullDayHr*FullTimeDay);
-		int part_time_incom=wageperHr*(HalfDayHr*partTimeDay);
+		int part_time_incom=(partTimeDay*(FullDayHr/2)*wageperHr);
+		System.out.println("**************emp wages*********************");
+		System.out.println("Full time Days in month:\t"+FullTimeDay);
+		System.out.println("Part time Days in month:\t"+partTimeDay);
+		System.out.println("Absent Days in month:\t\t"+absent);
+		System.out.println("Total working Days in month:\t"+(FullTimeDay+partTimeDay));
 		System.out.println("--------------------------------------------");
 		System.out.println("Employee Full Time Income="+ full_time_incom);
-		System.out.println("--------------------------------------------");
 		System.out.println("Employee part Time Income="+part_time_incom);
+		System.out.println("Total Salary:\t"+(full_time_incom+part_time_incom));
+		System.out.println("--------------------------------------------");
 		
 		
 	}
