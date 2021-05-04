a3951c70330cf289aeccea74a32c234a2291ca3e
Pratap Pawar <pratappawar1997@gmail.com>
2021-05-04 23:46:32 +0530

    Added: Ability to Save The Total Emp Wage for Each Company

--- src/com/empwage/EmployeeWageBuilder.java
+++ src/com/empwage/EmployeeWageBuilder.java
@@ -5,7 +5,21 @@ public class EmployeeWageBuilder {
 	public final static int IS_FULL_TIME = 1;
 	public final static int IS_PART_TIME = 0;
 
-	public static void empWageCalculation(String company, int empRatePerHr, int workingDayPerMonth, int maxHrInMonth) {
+	private final String company;
+	private final int empRatePerHr;
+	private final int workingDayPerMonth;
+	private final int maxHrInMonth;
+	private int totalEmpWage;
+
+	public EmployeeWageBuilder(String company, int empRatePerHr, int workingDayPerMonth, int maxHrInMonth) {
+		super();
+		this.company = company;
+		this.empRatePerHr = empRatePerHr;
+		this.workingDayPerMonth = workingDayPerMonth;
+		this.maxHrInMonth = maxHrInMonth;
+	}
+
+	public void empWageCalculation() {
 		int empHrs = 0, totalEmpHrs = 0, totalWorkingDay = 0;
 		// Using switch Case
 		while (totalEmpHrs < maxHrInMonth && totalWorkingDay < workingDayPerMonth) {
@@ -25,12 +39,20 @@ public class EmployeeWageBuilder {
 			totalEmpHrs += empHrs;
 			System.out.println("Total Working Days: " + totalWorkingDay + " " + "Total EMP Hours:" + totalEmpHrs);
 		}
-		System.out.println("Total Emp Wage For Company:-->" + company + " is:" + totalEmpHrs * empRatePerHr);
+		totalEmpWage = totalEmpHrs * empRatePerHr;	
+	}
+	public String toString() {
+		return "[Total Emp Wage for: "+company+" is: " + totalEmpWage + "]";
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation !!");
-		empWageCalculation("D-MART", 20, 2, 10);
-		empWageCalculation("JIO", 20, 2, 10);
+		EmployeeWageBuilder dmart = new EmployeeWageBuilder("D-MART", 20, 2, 10);
+		EmployeeWageBuilder jio = new EmployeeWageBuilder("JIO", 20, 2, 10);
+		dmart.empWageCalculation();
+		System.out.println(dmart);
+		jio.empWageCalculation();
+		System.out.println(jio);
+		
 	}
 }
